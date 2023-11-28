53182bc6326d12ea1cfd966a1cb9a4c048bd0e7f
fripexx <fripexx@gmail.com>
2023-11-28 14:07:39 +0200

    feat: Created core Redux store files and set up the provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {setupStore} from "./store/store";
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 reportWebVitals();
