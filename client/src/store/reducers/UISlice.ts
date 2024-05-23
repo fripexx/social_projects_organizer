9c582c52d381785d2a8d42378276a2846caca2e8
fripexx <76876066+fripexx@users.noreply.github.com>
2024-05-23 18:00:07 +0300

    Created GeneralChatSidebar component for displaying chat. Added showGeneralChat parameter and showChat function in UISlice to control chat display. Added clickCallback prop to ModalBackdrop component for handling clicks on the component.

--- client/src/store/reducers/UISlice.ts
+++ client/src/store/reducers/UISlice.ts
@@ -2,10 +2,12 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface UIState {
     showMobileSidebar: boolean,
+    showGeneralChat: boolean,
 }
 
 const initialState: UIState = {
     showMobileSidebar: false,
+    showGeneralChat: false
 }
 
 const UISlice = createSlice({
@@ -15,9 +17,12 @@ const UISlice = createSlice({
         showSidebar: (state, action: PayloadAction<boolean>) => {
             state.showMobileSidebar = action.payload;
         },
+        showChat: (state, action: PayloadAction<boolean>) => {
+            state.showGeneralChat = action.payload;
+        },
     },
     extraReducers: {}
 })
 
 export default UISlice.reducer;
-export const { showSidebar } = UISlice.actions;
+export const { showSidebar, showChat } = UISlice.actions;
