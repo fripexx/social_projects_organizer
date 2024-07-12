dd13edab793c680ac4d2f2b5abf19d8802f6b254
fripexx <76876066+fripexx@users.noreply.github.com>
2024-07-12 17:41:42 +0300

    refactor: Separated ChatThunks into separate files.

---
+++ client/src/api/services/ChatService.ts
@@ -0,0 +1,26 @@
+import instanceServer from "../instanceServer";
+import {isAxiosError} from "axios";
+import {ErrorResponseType} from "../types/ErrorResponseType";
+import {MessageType} from "../../store/types/MessageType";
+
+class ChatService {
+    async sendMessage(data: FormData): Promise<MessageType> {
+        try {
+            const response = await instanceServer.post<MessageType>('/send-message', data);
+            return response.data;
+        } catch (e) {
+            const response: ErrorResponseType = {
+                status: 0,
+                message: 'Непередбачена помилка',
+            };
+
+            if (isAxiosError(e) && e.response) {
+                response.status = e.response.status;
+                response.message = e.response.data.message;
+            }
+            throw response;
+        }
+    }
+}
+
+export default new ChatService();
--- client/src/store/thunks/ChatThunks.ts
+++ client/src/store/thunks/ChatThunks.ts
@@ -1,36 +1,15 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import instanceServer from "../../api/instanceServer";
+import ChatService from "../../api/services/ChatService";
 import {ErrorResponseType} from "../../api/types/ErrorResponseType";
-import {isAxiosError} from "axios";
 import {MessageType} from "../types/MessageType";
 
-interface SendMessageProps {
-    chat: string,
-    model: "Project" | "Post",
-    content: string,
-    files: File[]
-}
-
-export const sendMessage = createAsyncThunk(
+export const sendMessage = createAsyncThunk<MessageType, FormData, { rejectValue: ErrorResponseType }>(
     'chat/sendMessage',
-    async (data: FormData, thunkAPI) => {
+    async (data, thunkAPI) => {
         try {
-            const response = await instanceServer.post<MessageType>(
-                `/send-message`,
-                data
-            );
-            return response.data;
+            return await ChatService.sendMessage(data);
         } catch (e) {
-            const response: ErrorResponseType = {
-                status: 0,
-                message: "Непередбачена помилка"
-            }
-
-            if (isAxiosError(e) && e?.response) {
-                response.status = e.response.status;
-                response.message = e.response.data.message;
-            }
-            return thunkAPI.rejectWithValue(response);
+            return thunkAPI.rejectWithValue(e as ErrorResponseType);
         }
     }
 );
