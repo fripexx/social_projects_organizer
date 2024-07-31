20b6d9b59434ce42cd6635ddcfd5f89a224d822a
fripexx <76876066+fripexx@users.noreply.github.com>
2024-07-31 17:07:56 +0300

    feat: Created edit page for Instagram Stories.

---
+++ client/src/Pages/EditPostPages/EditInstagramStories/Components/InstagramStoriesParams/InstagramStoriesParams.tsx
@@ -0,0 +1,118 @@
+import React, {FC, useEffect, useState} from 'react';
+import classes from "./InstagramStoriesParams.module.scss";
+import classNames from "classnames";
+import Field from "../../../../../Elements/Field/Field";
+import InputTime from "../../../../../Elements/InputTime/InputTime";
+import InputDate from "../../../../../Elements/InputDate/InputDate";
+import SetMedia from "../../../../../Components/SetMedia/SetMedia";
+import {useAppDispatch, useAppSelector} from "../../../../../store/hooks/redux";
+import {setPost, setEdit, setSelectMedia} from "../../../../../store/reducers/InstagramStoriesSlice";
+import {setError} from "../../../../../store/reducers/ProjectSlice";
+import ProjectMediaService from "../../../../../api/services/ProjectMediaService";
+import {FileType, PhotoType} from "../../../../../store/types/FileType";
+import {QueryMediaRequestType} from "../../../../../api/types/ProjectMediaTypes";
+
+interface InstagramStoriesParamsProps {
+    className?: string;
+}
+
+const InstagramStoriesParams:FC<InstagramStoriesParamsProps> = ({className}) => {
+    const dispatch = useAppDispatch()
+
+    const project = useAppSelector(state => state.ProjectReducer.project)
+    const stories = useAppSelector(state => state.InstagramStoriesSlice.stories)
+    const selectMedia = useAppSelector(state => state.InstagramStoriesSlice.selectMedia)
+
+    const [mediaLibrary, setMediaLibrary] = useState<(PhotoType | FileType)[]>([])
+    const [total, setTotal] = useState<number>(0)
+    const [query, setQuery] = useState<QueryMediaRequestType>();
+    const [loadMore, setLoadMore] = useState<boolean>(false);
+
+
+    const changeDateHandler = (date: Date): void => {
+        if(stories) {
+            dispatch(setPost({...stories, datePublish: date.toISOString()}))
+            dispatch(setEdit(true))
+        }
+    }
+    const loadMoreCallback = () => {
+        if (!loadMore) setLoadMore(true)
+    }
+    const selectMediaHandler = (media: FileType): void => {
+        dispatch(setSelectMedia(media))
+        dispatch(setEdit(true))
+    }
+    const unselectMediaItemHandler = (removeId: string) => {
+        dispatch(setSelectMedia(undefined))
+        dispatch(setEdit(true))
+    }
+    const updateSelectMediaHandler = (updateMedia: FileType[]) => {
+        dispatch(setSelectMedia(updateMedia[0]))
+        dispatch(setEdit(true))
+    }
+    useEffect(() => {
+        if(project) {
+            setQuery({
+                projectId: project.id,
+                limit: 5,
+                skip: 0,
+                type: ['video']
+            })
+        }
+    }, [project]);
+    useEffect(() => {
+        if (project && query && loadMore) {
+            setQuery({
+                projectId: project.id,
+                limit: 5,
+                skip: mediaLibrary.length,
+                type: ['video']
+            })
+        }
+    }, [loadMore]);
+    useEffect(() => {
+        if (query) {
+            ProjectMediaService.getMedia(query)
+                .then((mediaData) => {
+                    if (mediaData) {
+                        setMediaLibrary(prevState => [...prevState, ...mediaData.media]);
+                        setTotal(mediaData.total)
+                        setLoadMore(false);
+                    }
+                })
+                .catch((error) => dispatch(setError(error)))
+        }
+    }, [query]);
+
+    if(stories) {
+        return (
+            <div className={classNames(classes.container, className)}>
+
+                <SetMedia
+                    mediaLibrary={mediaLibrary}
+                    total={total}
+                    title={"Оберіть медіа"}
+                    loadMoreCallback={loadMoreCallback}
+                    maxSelectCount={1}
+                    selectMedia={selectMedia ? [selectMedia] : []}
+                    selectCallback={selectMediaHandler}
+                    unselectCallback={unselectMediaItemHandler}
+                    updateMediaCallback={updateSelectMediaHandler}
+                />
+
+                <Field text={'Час публікації'}>
+                    <InputTime changeCallback={changeDateHandler} value={new Date(stories.datePublish)}/>
+                </Field>
+
+                <Field text={'Дата публікації'}>
+                    <InputDate changeCallback={changeDateHandler} value={new Date(stories.datePublish)}/>
+                </Field>
+
+            </div>
+        );
+    } else {
+        return <></>
+    }
+};
+
+export default InstagramStoriesParams;
--- client/src/Pages/EditPostPages/EditInstagramStories/EditInstagramStories.tsx
+++ client/src/Pages/EditPostPages/EditInstagramStories/EditInstagramStories.tsx
@@ -1,20 +1,179 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import classes from "./EditInstagramStories.module.scss";
-import {editButtons} from "../constants/editButtons";
 import ProjectPage from "../../../HOC/ProjectPage/ProjectPage";
 import Page from "../../../Components/Page/Page";
 import SidebarProject from "../../../Components/SidebarProject/SidebarProject";
 import ContentPage from "../../../Components/ContentPage/ContentPage";
 import HeaderPage from "../../../Components/HeaderPage/HeaderPage";
-import Button from "../../../Elements/Button/Button";
-import EditPostButtons from "../../../Components/EditPostComponents/EditPostButtons/EditPostButtons";
 import StatusPostLabel from "../../../Components/StatusPostLabel/StatusPostLabel";
 import Content from "../../../Components/Content/Content";
 import InstagramStoriesPreview
     from "../../../Components/InstagramComponents/InstagramStoriesPreview/InstagramStoriesPreview";
+import BackLink from "../Components/BackLink/BackLink";
+import EditButtons from "../Components/EditButtons/EditButtons";
+import Tabs from "../Components/Tabs/Tabs";
+import classNames from "classnames";
+import ChatPost from "../Components/ChatPost/ChatPost";
+import Loader from "../../../Elements/Loader/Loader";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {getInstagramStories, publishInstagramStories, updateInstagramStories} from "../../../store/thunks/PostThunks";
+import {useAppDispatch, useAppSelector} from "../../../store/hooks/redux";
+import {setClearPost, setPost, setSelectMedia} from "../../../store/reducers/InstagramStoriesSlice";
+import {setError} from "../../../store/reducers/ProjectSlice";
+import PostService from "../../../api/services/PostService";
+import ProjectMediaService from "../../../api/services/ProjectMediaService";
+import {InstagramStoriesType} from "../../../store/types/PostType";
+import {QueryMediaRequestType} from "../../../api/types/ProjectMediaTypes";
+import FileDownloader from "../../../utils/FileDownloader";
+import InstagramStoriesParams from "./Components/InstagramStoriesParams/InstagramStoriesParams";
 
 const EditInstagramStories:FC = () => {
-    const buttonsCallback = (key: string) => {}
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate();
+
+    const user = useAppSelector(state => state.UserReducer.user);
+    const project = useAppSelector(state => state.ProjectReducer.project);
+    const teamProject = useAppSelector(state => state.ProjectReducer.team);
+    const post = useAppSelector(state => state.InstagramStoriesSlice.stories)
+    const isEdit = useAppSelector(state => state.InstagramStoriesSlice.isEdit)
+    const selectMedia = useAppSelector(state => state.InstagramStoriesSlice.selectMedia)
+
+    const [currentTab, setCurrentTab] = useState<string>("preview")
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [isAuthor, setAuthor] = useState<boolean>(false);
+    const [isCustomer, setCustomer] = useState<boolean>(false);
+
+    const buttonsHandler = (key: string) => {
+        switch (key) {
+            case "publish":
+                publishCallback();
+                break;
+
+            case "delete":
+                deleteCallback();
+                break;
+
+            case "save":
+                saveCallback();
+                break;
+
+            case "download":
+                downloadCallback();
+                break;
+
+            case "send-for-confirmation":
+                sendForConfirmation();
+                break;
+
+            case "reject":
+                rejectCallback();
+                break;
+
+            case "confirm":
+                confirmCallback();
+                break;
+        }
+    }
+    const publishCallback = () => {
+        if(project && user && isAuthor) {
+            if(selectMedia !== undefined && post) {
+                dispatch(publishInstagramStories(post))
+                    .then((action) => {
+                        if (publishInstagramStories.fulfilled.match(action)) navigate(`?id=${action.payload.id}`);
+                    });
+            } else {
+                dispatch(setError({ message: "Додайте медіафайл" }))
+            }
+        }
+    }
+    const saveCallback = () => {
+        if(isEdit && isAuthor) {
+            if(project && user) {
+                if(selectMedia && post) {
+                    dispatch(updateInstagramStories(post))
+                } else {
+                    dispatch(setError({ message: "Додайте хоча б один медіафайл" }))
+                }
+            }
+        } else {
+            dispatch(setError({ message: "Ви не відредагували публікацію" }))
+        }
+    }
+    const deleteCallback = () => {
+        if (project && post?.id && isAuthor) {
+            PostService.deletePost(post.id)
+                .then((response) => {
+                    if (response === post.id) navigate(`/project/${project.id}/posts`)
+                })
+                .catch((error) => dispatch(setError(error)))
+        }
+    }
+    const downloadCallback = () => {
+        if(selectMedia) {
+            const fileDownloader = new FileDownloader(selectMedia);
+            fileDownloader.downloadFiles();
+        }
+    }
+    const sendForConfirmation = () => {
+        if(post?.id && isAuthor) {
+            PostService.sendForConfirmation(post.id)
+                .then((response) => dispatch(setPost(response as InstagramStoriesType)))
+                .catch((error) => dispatch(setError(error)))
+        }
+    }
+    const rejectCallback = () => {
+        if(post?.id && isCustomer) {
+            PostService.rejectPost(post.id)
+                .then((response) => dispatch(setPost(response as InstagramStoriesType)))
+                .catch((error) => dispatch(setError(error)))
+        }
+    }
+    const confirmCallback = () => {
+        if(post?.id && isCustomer) {
+            PostService.confirmPost(post.id)
+                .then((response) => dispatch(setPost(response as InstagramStoriesType)))
+                .catch((error) => dispatch(setError(error)))
+        }
+    }
+    const changeTab = (key: string) => {
+        setCurrentTab(key)
+    }
+
+    useEffect(() => {
+        if (searchParams && project && user && !post) {
+            const id = searchParams.get("id");
+            if (id) {
+                dispatch(getInstagramStories({project: project.id, id}))
+            } else {
+                dispatch(setClearPost({project: project.id, author: user.id}))
+            }
+        }
+    }, [searchParams, project, user]);
+    useEffect(() => {
+        if (project && post && post.params.media && !selectMedia) {
+
+            const query:QueryMediaRequestType = {
+                projectId: project.id,
+                limit: 1,
+                skip: 0,
+                mediaIds: [post.params.media]
+            }
+
+            ProjectMediaService.getMedia(query)
+                .then((mediaData) => {
+                    if (mediaData && mediaData.media.length !== 0) dispatch(setSelectMedia(mediaData.media[0]))
+                })
+                .catch(error => dispatch(setError(error)))
+        }
+    }, [post])
+    useEffect(() => {
+        if(post && user && project) {
+            const userInTeam = project.team.find((item) => item.user === user.id);
+
+            setCustomer(userInTeam?.role === "customer")
+            setAuthor(post.author  === user.id)
+        }
+    }, [project, user, post]);
 
     return (
         <ProjectPage>
@@ -24,31 +183,64 @@ const EditInstagramStories:FC = () => {
                 <SidebarProject/>
 
                 <ContentPage>
+                    {(project && post && user) ? (
+                        <>
+                            <HeaderPage className={classes.header}>
 
-                    <HeaderPage className={classes.header}>
+                                <BackLink className={classes.back} project={project.id}/>
 
-                        <Button text={"Назад"} onClick={() => {
-                        }}/>
+                                <StatusPostLabel className={classes.status} status={post.status}/>
 
-                        <EditPostButtons buttons={editButtons} callback={buttonsCallback}/>
+                                <EditButtons
+                                    className={classes.editButtons}
+                                    status={post.status}
+                                    callback={buttonsHandler}
+                                    isAuthor={isAuthor}
+                                />
 
-                        <StatusPostLabel status={'edit'}/>
+                            </HeaderPage>
 
-                    </HeaderPage>
+                            <Content className={classes.columns}>
 
-                    <Content className={classes.columns}>
+                                <Tabs
+                                    callback={changeTab}
+                                    activeTab={currentTab}
+                                />
 
-                        <InstagramStoriesPreview
-                            profile={{
-                                name: "",
-                                picture: ""
-                            }}
-                            media={undefined}
-                            width={300}
-                        />
+                                <div className={classNames(classes.column, classes.previewContainer)} data-active={currentTab === "preview"}>
+                                    
+                                   <InstagramStoriesPreview
+                                        profile={{
+                                            name: "",
+                                            picture: ""
+                                        }}
+                                        media={selectMedia}
+                                    />
 
-                    </Content>
+                                </div>
 
+                                <div className={classNames(classes.column, classes.paramsContainer)} data-active={currentTab === "params"}>
+                                    <InstagramStoriesParams className={classes.params}/>
+                                </div>
+
+                                <div className={classNames(classes.column, classes.chatContainer)} data-active={currentTab === "comments"}>
+
+                                    {post.status !== "unpublish" &&
+                                        <ChatPost
+                                            className={classes.chat}
+                                            post={post}
+                                            user={user}
+                                            team={teamProject}
+                                        />
+                                    }
+
+                                </div>
+
+                            </Content>
+                        </>
+                    ) : (
+                        <Loader/>
+                    )}
                 </ContentPage>
 
             </Page>
